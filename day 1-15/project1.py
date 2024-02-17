163284fa291d6eead804996042e3e4bb456ac686
sumanpokhrel-11 <11pksuman@gmail.com>
2024-02-17 11:11:59 +0545

    project 1 of day 1 is completed

--- day 1-15/project1.py
+++ day 1-15/project1.py
@@ -5,6 +5,6 @@ print('''
             your pet's name and 
       We will generates name for your Band''')
 city = str(input("\nEnter the name of your City: "))
-pet = str(input("\nEnter the name of your pet: "))
+pet = str(input("\nEnter the name of your pet:  "))
 # now based on that i'm going to name your band
 print("\n The name of your band is {} {}".format(city, pet))

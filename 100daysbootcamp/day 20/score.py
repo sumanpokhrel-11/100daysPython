62ec6eedac2796089032ea0056a73e3136f64422
sumanpokhrel-11 <11pksuman@gmail.com>
2024-02-22 14:40:21 +0545

    score card is added to the game

--- 100daysbootcamp/day 20/score.py
+++ 100daysbootcamp/day 20/score.py
@@ -1,5 +1,26 @@
 from turtle import Turtle
+ALIGN = 'center'
+FONT = ('Arial', 20, 'bold')
+FONT_GO = ('Arial', 50, 'bold')
 class Score(Turtle):
     def __init__(self):
         super().__init__()
+        self.score = 0
+        self.penup()
+        self.color('white')
+        self.goto(0,270)
+        self.hideturtle()
+        self.write_score()
+    def write_score(self):
+        self.write(f'Score : {self.score}', align=ALIGN, font=FONT)
         
+        
+    def increase_score(self):
+        self.score +=1
+        self.clear()
+        self.write_score()
+    
+    def game_over(self):
+        self.goto(0,0)
+        self.write("Game Over",align=ALIGN, font=FONT_GO)
+

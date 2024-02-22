1cdddf0054b1d4642e3db44d8e83595a373e2ca0
sumanpokhrel-11 <11pksuman@gmail.com>
2024-02-22 12:17:43 +0545

    main updated

--- 100daysbootcamp/day 20/main.py
+++ 100daysbootcamp/day 20/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 '''setting up the screen'''
 screen = Screen()
@@ -8,7 +9,7 @@ screen.title('Snake Game')
 screen.bgcolor('black')
 screen.tracer(0)
 snake = Snake()
-
+food = Food()
 '''setting up the key handler i.e arrows handles the directions'''
 screen.listen()
 screen.onkey(snake.up, 'Up')
@@ -23,6 +24,10 @@ while snake_alive:
     time.sleep(0.09)    
     snake.move_snake()        
 
+
+#detecting collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
    
 
 screen.exitonclick()

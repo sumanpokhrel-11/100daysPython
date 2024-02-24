f7ebacd148131b61a336207cf319761be5834f7a
sumanpokhrel-11 <11pksuman@gmail.com>
2024-02-24 10:49:17 +0545

    score card added

--- 100daysbootcamp/day 22/score_board.py
+++ 100daysbootcamp/day 22/score_board.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
 ALIGN = 'center'
-FONT = ('Arial', 20, 'bold')
+FONT = ('Arial', 40, 'bold')
 class Score(Turtle):
     def __init__(self):
         super().__init__()
@@ -9,10 +9,21 @@ class Score(Turtle):
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
+
+
+    def write_score(self):
+
+        self.clear()
         self.goto(-100, 200)
-        self.write(f'Left Score = {self.l_score}, Right Score is {self.r_score}', align =ALIGN , font=FONT)
+        self.write(f'{self.l_score}', align =ALIGN , font=FONT)
+
+        self.goto(100, 200)
+        self.write(f'{self.r_score}', align =ALIGN , font=FONT)
         
     def score_l(self):
         self.l_score +=1
+        self.write_score()
+
     def score_r(self):
         self.r_score +=1
+        self.write_score()

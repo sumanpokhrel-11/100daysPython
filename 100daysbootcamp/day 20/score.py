46439f1548e18fb80633e5f4702514dce1a2ef0c
sumanpokhrel-11 <11pksuman@gmail.com>
2024-03-03 23:12:18 +0545

    updated

--- 100daysbootcamp/day 20/score.py
+++ 100daysbootcamp/day 20/score.py
@@ -6,21 +6,26 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.color('white')
         self.goto(0,270)
         self.hideturtle()
         self.write_score()
+
     def write_score(self):
-        self.write(f'Score : {self.score}', align=ALIGN, font=FONT)
+        self.clear()
+        self.write(f'Score : {self.score}  High Score: {self.high_score}', align=ALIGN, font=FONT)
         
         
     def increase_score(self):
         self.score +=1
-        self.clear()
+        
+        self.write_score()
+
+    def game_reset(self):
+        if self.score>self.high_score:
+            self.high_score = self.score
+        self.score = 0
         self.write_score()
     
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over",align=ALIGN, font=FONT_GO)
-

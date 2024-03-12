a42a89ec16f48c0ff3493c44ca63b30e490b3db6
sumanpokhrel-11 <11pksuman@gmail.com>
2024-03-12 23:42:30 +0545

    learning kwargs in python

--- 100daysbootcamp/day 27/learning_tk.py
+++ 100daysbootcamp/day 27/learning_tk.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+# learning tkinter basics
 window = tk.Tk()
 window.title("Learning Tkinter")
 window.minsize(width=500, height= 800)
@@ -17,3 +17,14 @@ def add(*num):
         sum +=i
     return print(sum)
 add(2, 3, 5, 2,7)
+
+# learning **kwargs
+class Car:
+    def __init__(self, **kw):
+        # use get method to use for optional argument
+        self.model = kw.get('model')
+        self.make = kw.get('make')
+        self.color = kw.get('color')
+        self.seats = kw['seats'] #this could raise an error if not initialize
+car = Car(make='Nissan', model = 'jiplines', seats =7)
+print(car.seats)

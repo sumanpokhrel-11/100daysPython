afc10d42e56dda3e313667af1323ad3b6e835507
sumanpokhrel-11 <11pksuman@gmail.com>
2024-03-12 23:46:38 +0545

    tkinter learning

--- 100daysbootcamp/day 27/learning_tk.py
+++ 100daysbootcamp/day 27/learning_tk.py
@@ -10,21 +10,3 @@ label.pack()
 window.mainloop()
 
 
-# learning *agr 
-def add(*num):
-    sum = 0
-    for i in num:
-        sum +=i
-    return print(sum)
-add(2, 3, 5, 2,7)
-
-# learning **kwargs
-class Car:
-    def __init__(self, **kw):
-        # use get method to use for optional argument
-        self.model = kw.get('model')
-        self.make = kw.get('make')
-        self.color = kw.get('color')
-        self.seats = kw['seats'] #this could raise an error if not initialize
-car = Car(make='Nissan', model = 'jiplines', seats =7)
-print(car.seats)

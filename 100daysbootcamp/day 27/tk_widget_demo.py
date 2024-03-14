757bf2ff3188a32cdd3b2e5a81b8e9b46618957b
sumanpokhrel-11 <11pksuman@gmail.com>
2024-03-14 21:17:12 +0545

    learning tkinter widget

--- 100daysbootcamp/day 27/tk_widget_demo.py
+++ 100daysbootcamp/day 27/tk_widget_demo.py
@@ -49,3 +49,26 @@ def scale_used(value):
     print(value)
 scale = Scale(from_=0, to=100, command=scale_used)
 scale.pack()
+
+#Checkbutton
+def checkbutton_used():
+    #Prints 1 if On button checked, otherwise 0.
+    print(checked_state.get())
+#variable to hold on to checked state, 0 is off, 1 is on.
+checked_state = IntVar()
+checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
+checked_state.get()
+checkbutton.pack()
+
+#Radiobutton
+def radio_used():
+    print(radio_state.get())
+#Variable to hold on to which radio button value is checked.
+radio_state = IntVar()
+radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+radiobutton1.pack()
+radiobutton2.pack()
+
+
+

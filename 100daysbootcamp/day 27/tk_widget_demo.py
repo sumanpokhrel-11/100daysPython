123bf95924911b8945e8bab77011680ff201d43a
sumanpokhrel-11 <11pksuman@gmail.com>
2024-03-14 21:14:55 +0545

    learning tkinter widget

---
+++ 100daysbootcamp/day 27/tk_widget_demo.py
@@ -0,0 +1,38 @@
+from tkinter import *
+
+#Creating a new window and configurations
+window = Tk()
+window.title("Widget Examples")
+window.minsize(width=500, height=500)
+
+#Labels
+label = Label(text="This is old text")
+label.config(text="This is new text")
+label.pack()
+
+#Buttons
+def action():
+    print("Do something")
+
+#calls action() when pressed
+button = Button(text="Click Me", command=action)
+button.pack()
+
+#Entries
+entry = Entry(width=30)
+#Add some text to begin with
+entry.insert(END, string="Some text to begin with.")
+#Gets text in entry
+print(entry.get())
+entry.pack()
+
+#Text
+text = Text(height=5, width=30)
+#Puts cursor in textbox.
+text.focus()
+#Adds some text to begin with.
+text.insert(END, "Example of multi-line text entry.")
+#Get's current value in textbox at line 1, character 0
+print(text.get("1.0", END))
+text.pack()
+

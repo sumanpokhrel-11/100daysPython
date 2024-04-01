7d7391fcd48542b7d94c5b07d5813a512c10e0d8
sumanpokhrel-11 <11pksuman@gmail.com>
2024-04-01 22:12:02 +0545

    timer app updated

--- 100daysbootcamp/day 28/main.py
+++ 100daysbootcamp/day 28/main.py
@@ -12,7 +12,7 @@ from tkinter import *
 import math
 # timer mechanism
 def start_timer():
-    count_down(5*60)
+    count_down(15)
 
 
 
@@ -20,6 +20,9 @@ def start_timer():
 def count_down(count):
     count_min = math.floor(count/60)
     count_sec = count%60
+
+    if count_sec <10:
+        count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text= f"{count_min}:{count_sec}")
     if count>0:
         window.after(1000, count_down, count-1)

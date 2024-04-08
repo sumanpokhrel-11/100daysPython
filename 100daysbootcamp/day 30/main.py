d548a033c8d8e2f49500d3a059502654fe90f876
sumanpokhrel-11 <11pksuman@gmail.com>
2024-04-08 10:48:55 +0545

    learning about exception handling in python

---
+++ 100daysbootcamp/day 30/main.py
@@ -0,0 +1,36 @@
+# exception handling and errors
+fruits = ["Apple", "Pear", "Orange"]
+
+# TODO: Catch the exception and make sure the code runs without crashing.
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+    except:
+        print("Fruit Pie")
+    else:
+        print(fruit + " pie")
+
+make_pie(4)
+
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2}, 
+    {'Likes': 13, 'Comments': 2, 'Shares': 1}, 
+    {'Likes': 33, 'Comments': 8, 'Shares': 3}, 
+    {'Comments': 4, 'Shares': 2}, 
+    {'Comments': 1, 'Shares': 1}, 
+    {'Likes': 19, 'Comments': 3}
+]
+
+total_likes = 0
+
+for post in facebook_posts:
+    try:
+        total_likes = total_likes + post['Likes']
+    except KeyError:
+        post['Likes'] = 0
+        # pass
+        # total_likes +=0
+
+
+print(total_likes)
+

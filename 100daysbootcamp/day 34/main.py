b5d9aa2e61204b40f57dd5cc210a19ab02914e65
sumanpokhrel-11 <11pksuman@gmail.com>
2024-04-18 01:02:40 +0545

    quiz app is updated

--- 100daysbootcamp/day 34/main.py
+++ 100daysbootcamp/day 34/main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizUI
 
 question_bank = []
 for question in question_data:
@@ -11,9 +12,9 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
-
-while quiz.still_has_questions():
-    quiz.next_question()
+quiz_ui = QuizUI()
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")

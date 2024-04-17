2045829d805f01d9828da8ecb7af8e1cc373b9da
sumanpokhrel-11 <11pksuman@gmail.com>
2024-04-18 01:02:13 +0545

    tkinter is updated for the app

--- 100daysbootcamp/day 34/ui.py
+++ 100daysbootcamp/day 34/ui.py
@@ -1,2 +1,12 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizUI:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quiz App")
+        
+
+
+        self.window.mainloop()

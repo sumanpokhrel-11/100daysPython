e7b732c62aa5b47693f86567e76fddd2a9c26057
sumanpokhrel-11 <11pksuman@gmail.com>
2024-04-20 23:37:31 +0545

    questions part upgraded

--- 100daysbootcamp/day 34/quiz_brain.py
+++ 100daysbootcamp/day 34/quiz_brain.py
@@ -14,8 +14,9 @@ class QuizBrain:
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
         q_text = html.unescape(self.current_question.text)
-        user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
-        self.check_answer(user_answer)
+        return f"Q.{self.question_number}: {q_text} (True/False): "
+        # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
+        # self.check_answer(user_answer)
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
@@ -27,3 +28,6 @@ class QuizBrain:
 
         print(f"Your current score is: {self.score}/{self.question_number}")
         print("\n")
+
+
+    

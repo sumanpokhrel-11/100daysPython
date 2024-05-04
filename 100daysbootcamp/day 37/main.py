31f7f3023aef68f07be9f5bbb8e042574567a1f3
sumanpokhrel-11 <11pksuman@gmail.com>
2024-05-05 00:39:10 +0545

    habit tracking using api key is created where you can put, post, delete and get data

--- 100daysbootcamp/day 37/main.py
+++ 100daysbootcamp/day 37/main.py
@@ -1,4 +1,5 @@
 import requests
+import datetime
 TOKEN = "gahganigah343hgh1"
 USERNAME = "sumanpok11"
 GRAPH_ID = "graph1"
@@ -36,10 +37,25 @@ header = {
 
 # step 3
 three_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
-
+today = datetime.datetime.now()
 three_params = {
-    "date" : "20240504",
-    "quantity": "39",
+    "date" : today.strftime("%Y%m%d"),
+    "quantity": input("Enter how many steps did you walk today by dividing it with 100: "),
 }
 response = requests.post(url=three_endpoint, json=three_params, headers = header)
 print(response.text)
+
+
+# update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+# new_pixel_data = {
+#     "quantity": "100"
+# }
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=header)
+# print(response.text)
+
+
+# delete_endpoint = update_endpoint
+
+# response = requests.delete(url=delete_endpoint, headers=header)
+# print(response.text)

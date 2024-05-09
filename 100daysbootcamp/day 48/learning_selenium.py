a2bc628a6bcadec59578908d25e07ee241f946db
sumanpokhrel-11 <11pksuman@gmail.com>
2024-05-09 23:47:43 +0545

    learning selenium web driver

--- 100daysbootcamp/day 48/learning_selenium.py
+++ 100daysbootcamp/day 48/learning_selenium.py
@@ -3,10 +3,11 @@ chrome_path = "C:\Development\chromedriver.exe"
 
 
 driver = webdriver.Chrome()
-driver.get("https://www.amazon.com")
+driver.get("https://www.python.org/")
 
-# closes the tab
+# close the tab using close and for browser use quit method
 # driver.close()
-
+donate = driver.find_element(by = "class_name", value="donate-button")
+print(donate.text)
 # closes the entire browser
 driver.quit()

a669b89f5098e3222ecdfa0021c1a60867780fc8
sumanpokhrel-11 <11pksuman@gmail.com>
2024-06-05 19:19:42 +0545

    basics of selenium done

--- 100daysbootcamp/day 48/task1.py
+++ 100daysbootcamp/day 48/task1.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 
@@ -16,4 +17,36 @@ for index in range(1, 6):
     }
 
 print(events)
+
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+num = driver.find_element(By.XPATH, '//*[@id="articlecount"]/a[1]').text
+
+
+# we can select specific link by using link_text and the name in the list 
+links = driver.find_element(By.LINK_TEXT, num)
+links.click()
+# while True:
+#     pass
+
+search = driver.find_element(By.NAME, "search")
+search.send_keys("Python")
+search.send_keys(Keys.RETURN)
+
+driver.get("https://secure-retreat-92358.herokuapp.com/")
+
+
+fname = driver.find_element(By.NAME, 'fName')
+fname.send_keys("suman")
+
+lname = driver.find_element(By.NAME, 'lName')
+lname.send_keys("pokhrel")
+
+email = driver.find_element(By.NAME, 'email')
+email.send_keys("11pok@gmail.com")
+
+submit = driver.find_element(By.XPATH, '/html/body/form/button')
+submit.click()
+
+# while True:
+#     pass
 
